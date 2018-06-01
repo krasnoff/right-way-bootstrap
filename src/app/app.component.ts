3fd9bbbca7e90ba31ab4fbdf4c22d9c0cb2d55ea
krasnoff <krasnoff.kobi@gmail.com>
2018-06-01 15:11:33 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, AfterViewChecked } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  ngAfterViewChecked() {
+    // here we call jquery functions
+  }
 }
